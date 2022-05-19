71a8943a00a24d4165521b2cebbc2da1dc40664e
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-20 00:59:08 +0900

    과제2: 게시글관리프로그램 메인

---
+++ Homework/src/Homework02_PostProgram/MainPostProgram.java
@@ -0,0 +1,15 @@
+package Homework02_PostProgram;
+
+import java.util.Scanner;
+
+public class MainPostProgram {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		PostProgramManager ppm = new PostProgramManager(scan);
+		ppm.run();
+		scan.close();
+
+	}
+
+}
