895aedff437b415100fc9f3c2b3d374f491d556e
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-20 23:44:34 +0900

    과제2: 게시글 클래스_수정1

--- Homework/src/Homework02_PostProgram/Post.java
+++ Homework/src/Homework02_PostProgram/Post.java
@@ -1,137 +1,110 @@
 package Homework02_PostProgram;
 
+import java.text.*;
+import java.util.*;
+
 public class Post implements Cloneable {
 	//필드
 	private String title; //게시글 제목
-	private String date; //작성 날짜
+	private Date date; //작성 날짜
 	private String id; //익명게시글이라서 아이디로
+	private String pw; //게시글 수정, 삭제할 때 비밀번호
 	private String content; //게시글 내용
 	private int view; //조회수
+	private int num; //게시글 번호
 	
-	//생성자1
-	public Post(String title, String date, String id, String content) {
+	
+	private static int count = 0;
+	
+	//생성자1 -> 일반글용
+	public Post(String title, String id, String pw, String content) {
+		this.num = ++count;
 		this.title = title;
-		this.date = date;
+		this.date = new Date();
 		this.id = id;
+		this.pw = pw;
 		this.content = content;
-	}//
-	
-	//생성자2
-	public Post(String title) {
-		this.title = title;
-	}//
-	
-	//기능1) 수정
-	public void modify(String title, String date, String id, String content) {
-		this.title = title;
-		this.date = date;
-		this.id = id;
-		this.content = content;
-	}//
-	
-	//기능2) 제목 출력
-	public void printTitle() {
-		System.out.println(title);
-	}
 		
+	}//
+	
+	//생성자2 -> 공지글용
+	public Post(String title, String content) {
+		this.num = ++count;
+		this.title = title;
+		this.date = new Date();
+		this.id = "Manager";
+		this.pw = "manager";
+		this.content = content;		
+	}//
+		
+	//기본생성자
+	public Post() {}//
+
+	//기능1) 수정
+	public void modify(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}//modify
+	
+	//기능2) 상세 출력
+	public void printDetail() {
+		System.out.println("=====================");
+		System.out.println("번호 : " + num);
+		System.out.println("제목 : " + title);
+		System.out.println("작성자 : " + id);
+		System.out.println("작성일 : " + getDate());
+		System.out.println("조회수 : " + view);
+		System.out.println("내용 : " + content);
+		System.out.println("=====================");		
+	}//printDetail
+	
+	//기능3) 조회수 증가
+	public void updateView(){
+		view++;
+	}//updateView
+	
+	//기능4) 번호 감소
+	public void reduceNum(){
+		num--;
+	}//reduceNum
+	
+	public static void reduceCount() {
+		count--;
+	}
 	//toString
 	@Override
 	public String toString() {
-		return  title + "\n 날짜 : " + date + ", 작성자 : " + id + ", 조회수 : " + view + "\n[내용]\n" + content + "";
+		DecimalFormat df = new DecimalFormat("000");
+		return df.format(num)+ " | " + title + " | " + id 
+				+ " | "+ getDate() + " | " + view +" views";
 	}//
 	
 	//getter and setter
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public String getDate() {
-		return date;
+		if(date == null) {
+			return null;
+		}
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		return format.format(date);	
+	}
+	public String getPw() {
+		return pw;
 	}
 
-	public void setDate(String date) {
-		this.date = date;
+	public static int getCount() {
+		return count;
+	}
+
+	public static void setCount(int count) {
+		Post.count = count;
 	}
 
 	public String getId() {
 		return id;
 	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
 	
-	public int getView() {
-		return view;
-	}
-
-	public void setView(int view) {
-		this.view = view;
-	}//
+	//
 	
-	//clone
-	public Post clone() {
-		try {
-			Post post = (Post)super.clone();
-			//post.id = new String(id);
-			return post;
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-			return null;
-		}	
-	}//
-
-	//hashcode
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((content == null) ? 0 : content.hashCode());
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		result = prime * result + view;
-		return result;
-	}//
-
-	//equals
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Post other = (Post) obj;
-		if (content == null) {
-			if (other.content != null)
-				return false;
-		} else if (!content.equals(other.content))
-			return false;
-		if (date == null) {
-			if (other.date != null)
-				return false;
-		} else if (!date.equals(other.date))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		if (view != other.view)
-			return false;
-		return true;
-	}//
 	
 	
 	
