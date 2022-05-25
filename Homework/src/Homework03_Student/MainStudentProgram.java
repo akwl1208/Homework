debd7f30aeaceac2bad0a4c649a78dbd32b9086c
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-25 09:01:22 +0900

    과제3: 메인학생관리프로그램

---
+++ Homework/src/Homework03_Student/MainStudentProgram.java
@@ -0,0 +1,17 @@
+package Homework03_Student;
+
+import java.util.*;
+
+public class MainStudentProgram {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		StudentProgramManager spm = new StudentProgramManager(scan);
+		spm.run();
+		
+		scan.close();
+
+	}
+
+}
